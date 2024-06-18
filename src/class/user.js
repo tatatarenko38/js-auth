bd915da6ba01174cb453327d7d16b3fe4d342771
Татьяна Татаренко <tatatatarenko38@gmail.com>
2024-06-19 00:43:43 +0300

    1

---
+++ src/class/user.js
@@ -0,0 +1,49 @@
+class User {
+  //статичне поле конст з визначеними ролями
+  // 1 2 3 - ідентифікатори, які будуть вказуватись на
+  //фронтенді для запиту на сервер
+  static USER_ROLE = {
+    USER: 1,
+    ADMIN: 2,
+    DEVELOPER: 3,
+  }
+
+  // список створених користувачів
+  static #list = []
+
+  //створюємо конструктор та підключаємо значення
+  constructor({ email, password, role }) {
+    this.email = email
+    this.password = password
+    //під'єднуємо з User.#convertRole()
+    this.role = User.#convertRole(role)
+  }
+
+  // конвертує перевіряє роль
+  static #convertRole = (role) => {
+    role = Number(role)
+
+    // якщо не число,
+    //то ставимо юзер за замовчуванням
+    if (isNaN(role)) {
+      role = this.USER_ROLE.USER
+    }
+
+    // чи є така роль в списку, якщо ні,
+    //то ставимо юзер за замовчуванням
+    role = Object.values(this.USER_ROLE).includes(role)
+      ? role
+      : this.USER_ROLE.USER
+
+    return role
+  }
+
+  //приймає данні, створює юзера і додає в список
+  static create(data) {
+    const user = new User(data)
+
+    this.#list.push(user)
+  }
+}
+
+module.exports = { User }
--- src/route/index.js
+++ src/route/index.js
@@ -2,8 +2,30 @@
 const express = require('express')
 const router = express.Router()
 
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('index', {
+    // вказуємо назву контейнера
+    name: 'index',
+    // вказуємо назву компонентів
+    component: [],
+
+    // вказуємо назву сторінки
+    title: 'Home page',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {},
+  })
+})
+
 // Підключіть файли роутів
-const test = require('./test')
+const auth = require('./auth')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
